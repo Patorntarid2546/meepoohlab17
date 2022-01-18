70c3453c3f1f4c13136b680d68663a86fb981c71
Patorntarid2546 <chpooha@gmail.com>
2022-01-18 21:44:42 +0700

    finish

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,56 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void randData(double *q, int N, int M)
+{
+	for(int i=0; i < N*M; i++)
+	{
+		q[i] = (rand()%101)/100.00;
+	}
+}
+void showData(double *w,int N,int M)
+{
+	cout << fixed << setprecision(2);
+	for(int i=0; i < N*M; i++)
+	{
+		cout << w[i];
+		if((i+1)%M == 0)
+		cout << endl;
+		else cout << " ";
+	}
+}
+void findRowSum(const double *a,double *b,int N,int M){
+	double sum1 = 0.00;
+	for(int i=0; i<N ; i++){
+		for(int j=0; j<M ; j++){
+			sum1 = sum1 + *(a+j+i*M);
+		}
+		*(b+i)=sum1;
+		sum1=0;
+	}
+}
+void findColSum(const double *Z,double *X,int C,int V)
+{
+	if(C == 1)
+	{
+		for(int i=0; i<V; i++)
+		{
+			X[i] = Z[i]; 
+		}
+	}else
+	{
+		for(int j = 0; j<C*V; j++)
+		{
+			X[j % V] = X[j % V] + Z[j];
+		}
+		for(int k = 0; k<V; k++)
+		{
+			X[k] = 0;
+		}
+		for(int m = 0; m<C*V; m++)
+		{
+			X[m%V] = X[m%V] + Z[m];
+		}
+	}
+}
